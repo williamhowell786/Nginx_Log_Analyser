3e63a97eeb5a8263241e0d6104b72c5f72db28bb
William Howell <williamhowell786@proton.me>
2025-11-17 14:51:27 -0500

    add nginx_log_analyser.sh

---
+++ nginx_log_analyser.sh
@@ -0,0 +1,8 @@
+echo "TOP 5 IP ADDRESSES WITH THE MOST REQUESTS:"
+awk '{print $1}' $1 | sort | uniq -ci | sort -nr | head -n 5
+echo "TOP 5 MOST REQUESTED PATHS:"
+awk '{print $7}' $1 | sort | uniq -ci | sort -nr | head -n 5
+echo "TOP 5 RESPONSE STATUS CODES:"
+awk '{print $9}' $1 | grep -v "-" | sort | uniq -ci | sort -nr | head -n 5
+echo "TOP 5 USER AGENTS:"
+awk '{$1=$2=$3=$4=$5=$6=$7=$8=$9=$10=$11=""; $0=$0} NF=NF' $1 | sort | uniq -ci | sort -nr | head -5
